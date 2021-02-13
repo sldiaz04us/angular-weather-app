df75a5d638b4be980fd53a3605776d9d5c7f66fa
Sergio Lopez Diaz <sldiaz04us@gmail.com>
2021-02-12 20:15:30 -0600

    refactor(view): lighthouse best practices and accessibility

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
+  isOpen = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  close(): void {
+    if (this.isOpen) { this.isOpen = !this.isOpen; }
   }
 
 }
