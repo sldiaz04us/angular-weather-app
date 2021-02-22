6a45a072ec747ae2cdd4326cb9b8fb2cf0d979ac
Sergio Lopez Diaz <sldiaz04us@gmail.com>
2021-02-22 14:23:15 -0600

    perf(app): cache user's location
    Cache the user's location for up to 10 minutes, which is when the
    OpenWeather API returns updated weather data.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { POSITION_OPTIONS } from '@ng-web-apis/geolocation';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
@@ -22,6 +24,10 @@ export function geoLocationFactory(geoLocationService: GeolocationApiService
     CoreModule,
   ],
   providers: [
+    {
+      provide: POSITION_OPTIONS,
+      useValue: { enableHighAccuracy: false, timeout: 4000, maximumAge: 600000 /* 10 minutes */ },
+    },
     {
       provide: APP_INITIALIZER,
       useFactory: geoLocationFactory,
